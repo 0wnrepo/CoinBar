e9a7812d0ba64a0a151064961a149b965a37c4c5
Benny Lach <benny@discollect.me>
2017-07-29 01:32:50 +0200

    Initial Commit

---
+++ CoinBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CoinBar
+//
+//  Created by Benny Lach on 29.07.17.
+//  Copyright © 2017 DevKiste. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CoinBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoinBar
+//
+//  Created by Benny Lach on 29.07.17.
+//  Copyright © 2017 DevKiste. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
